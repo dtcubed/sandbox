f48a2be27c6aabc93f18f8f408e64eb6b8837fef
dtcubed <dtcubed@nowhere.com>
2011-10-29 12:21:15 -0500

    ET Experiment 3

---
+++ exp03-et/EtClientLib/EtClientLib.java
@@ -0,0 +1,16 @@
+/**
+** Title:                EtClientLib.java
+**/
+
+public class EtClientLib {
+
+    public static void main(String[] args) {
+
+        String msg = "";
+
+        msg += "EtClient program - Hello World!\n";
+
+        System.out.print(msg);
+
+    }
+}
---
+++ exp03-et/EtMiddleware/EtMiddleware.java
@@ -0,0 +1,16 @@
+/**
+** Title:                EtMiddleware.java
+**/
+
+public class EtMiddleware {
+
+    public static void main(String[] args) {
+
+        String msg = "";
+
+        msg += "EtMiddleware program - Hello World!\n";
+
+        System.out.print(msg);
+
+    }
+}
